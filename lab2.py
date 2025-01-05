3ccd8583f2a8c58b12faf33934041e7768a9d996
9swamp9god9 <141263599+9swamp9god9@users.noreply.github.com>
2025-01-05 23:38:49 +0400

    2 лабораторная работа

--- lab2.py
+++ lab2.py
@@ -1,40 +1,40 @@
 # задание 1
 
-# name = str(input())
-# def greet(name):
-#     print('Привет', name)
-# greet(name)
-#
-# number = int(input())
-# def square(number):
-#     print(number**2)
-# square(number)
-#
-#
-# x = int(input())
-# y = int(input())
-# def max_of_two(x,y):
-#     if x>y:
-#         print(x)
-#     elif x==y:
-#         print('Числа равны')
-#     else:
-#         print(y)
-# max_of_two(x,y)
-#
-#
-# number = int(input())
-# def is_prime(number):
-#     count = 0
-#     for i in range(2,number):
-#         if number%i == 0:
-#             count += 1
-#         if count != 0 :
-#             return False
-#         else:
-#             return True
-# print(is_prime(number))
-#
+ name = str(input())
+ def greet(name):
+     print('Привет', name)
+ greet(name)
+
+ number = int(input())
+ def square(number):
+     print(number**2)
+ square(number)
+
+
+ x = int(input())
+ y = int(input())
+ def max_of_two(x,y):
+     if x>y:
+         print(x)
+     elif x==y:
+         print('Числа равны')
+     else:
+         print(y)
+ max_of_two(x,y)
+
+
+ number = int(input())
+ def is_prime(number):
+     count = 0
+     for i in range(2,number):
+         if number%i == 0:
+             count += 1
+         if count != 0 :
+             return False
+         else:
+            return True
+print(is_prime(number))
+
 name = str(input('Введите имя: '))
 age = int(input('Введите возраст: '))
 
